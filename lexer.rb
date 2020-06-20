f7fca2466b541d85c0d3d23f73ded63e484f662f
vladyslavnUA <vladyslavn@yahoo.com>
2020-06-19 20:54:58 -0400

    lexer test

--- lexer.rb
+++ lexer.rb
@@ -53,3 +53,58 @@ class Lexer
             #     line 2 ne indented line
             # continue dedent
         #######################################
+        
+        elsif indent = chunk[/\A\:\n( +)/m, 1]  # Matches: "<newline> <spaces>"
+            # error case, trace indentation level
+            if indent.size <= current_indent
+                raise "Bad indent level. Program has #{indent.size} indents. " +
+                "expected > #{current_indent}"
+            end
+
+            current_indent = indent.size
+            indent_stack.push(current_indent)
+            tokens << [:INDENT, indent.size]
+            i += indent.size + 2
+        
+        # Case: code is in the same block if the indent level is the same as current_indent
+        # Case: if indent level is lower than current_indent. close the block
+        elsif indent = chunk[/\A\n( *)/m, 1]
+            if indent.size == current_indent
+                tokens << [:NEWLINE, "\n"]
+        elsif indent.size < current_indent
+            while indent.size < current_indent
+                indent_stack.pop
+                current_indent = indent_stack.first || 0
+                tokens << [:DEDENT, indent.size]
+            end
+            tokens << [:NEWLINE, "\n"]
+        else
+            raise "Missing ':' indentation"
+        end
+        i += indent.size + 1
+
+        # Dealing with operators: ||, &&, ==, !=. <= and >=
+        elsif operator = chunk[/\A(\|\||&&|==|!=|<=|>=)/, 1]
+            tokens << [operator, operator]
+            i += operator.size
+        
+        # whitespace
+        elsif chunk.match(/\A /)
+            i += 1
+
+            # single characters: ( ) , . ! + - < > =
+            else
+                value = chunk[0,1]
+                tokens << [value, value]
+                i += 1
+            end
+        end
+
+        # close open blocks
+        while indent = indent_stack.pop
+            tokens << [:DEDENT, indent_stack.first || 0]
+        end
+
+        tokens
+    end
+end
